4df9ce1cda018a5f25f93d71cf6d014b9d009f3a
titus njiru <njirutitus@gmail.com>
2021-07-23 17:38:53 +0300

    Adding close,execute_action and menu functions

--- main.c
+++ main.c
@@ -10,22 +10,66 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <windows.h>
+
+// Function prototype
+void execute_action(int action);
+void close();
+int menu();
+
 int main()
 {
     int action;
-    printf("\tCounty Library Management System!\n");
-    printf("Welcome Titus\n");
+    action = menu();
+    execute_action(action);
+
+    return 0;
+}
+
+void execute_action(int action) {
+    switch(action) {
+    case 1:
+        printf("Adding a user\n");
+        break;
+    case 2:
+        printf("Adding a book\n");
+        break;
+    case 3:
+        close();
+        break;
+    default:
+        printf("Unknown action. \n");
+
+    }
+
+}
+
+void close() {
+    printf("Thank you for using the system\n");
+    printf("Bye\n");
+    Sleep(5000);
+    exit(0);
+}
+
+int menu() {
     // Navigation menu
+    int action;
     do {
+        printf("\tCounty Library Management System!\n");
+        printf("Welcome Titus\n");
         printf("1. Add user\n");
         printf("2. Add Book\n");
         printf("3. Exit\n");
         printf("Select action(1-3): ");
         scanf("%d",&action);
         // validate input
-        if (action < 1 || action > 3)
+        if (action < 1 || action > 3) {
             printf("Invalid action. Try again\n");
+            Sleep(2000);
+            system("cls");
+        }
+
     } while(action < 1 || action > 3);
 
-    return 0;
+    return action;
 }
