5b161c44431ae39b84637018716df7d2f89be476
titus njiru <njirutitus@gmail.com>
2021-08-11 21:22:15 +0300

    adding add_user, view_users actions implementations with file storage

--- main.c
+++ main.c
@@ -11,17 +11,42 @@
 #include <math.h>
 #include <string.h>
 #include <windows.h>
+#include <conio.h>
+
+// Adding user:
+// name
+// id
+// contact
+// is_staff
+
+struct user {
+    char name[100];
+    int id;
+    char tel[13];
+    int is_staff;
+};
+
+
 
 // Function prototype
 void execute_action(int action);
 void close();
 int menu();
+void add_user();
+void view_users();
+void delete_user();
+void update_user();
 
 int main()
 {
     int action;
-    action = menu();
-    execute_action(action);
+    while(1) {
+        action = menu();
+        execute_action(action);
+        printf("\nPress any key to continue");
+        getch();
+        system("cls");
+    }
 
     return 0;
 }
@@ -29,12 +54,15 @@ int main()
 void execute_action(int action) {
     switch(action) {
     case 1:
-        printf("Adding a user\n");
+        add_user();
         break;
     case 2:
-        printf("Adding a book\n");
+        view_users();
         break;
     case 3:
+        printf("Adding a book\n");
+        break;
+    case 4:
         close();
         break;
     default:
@@ -47,7 +75,7 @@ void execute_action(int action) {
 void close() {
     printf("Thank you for using the system\n");
     printf("Bye\n");
-    Sleep(5000);
+    Sleep(2000);
     exit(0);
 }
 
@@ -58,18 +86,68 @@ int menu() {
         printf("\tCounty Library Management System!\n");
         printf("Welcome Titus\n");
         printf("1. Add user\n");
-        printf("2. Add Book\n");
-        printf("3. Exit\n");
-        printf("Select action(1-3): ");
+        printf("2. View users\n");
+        printf("3. Add Book\n");
+        printf("4. Exit\n");
+        printf("Select action(1-4): ");
         scanf("%d",&action);
         // validate input
-        if (action < 1 || action > 3) {
+        if (action < 1 || action > 4) {
             printf("Invalid action. Try again\n");
             Sleep(2000);
             system("cls");
         }
 
-    } while(action < 1 || action > 3);
+    } while(action < 1 || action > 4);
 
     return action;
+}
+
+void add_user() {
+    struct user u;
+    FILE *fp;
+    if ((fp = fopen("users","ab"))==NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    printf("Name: ");
+    getchar();
+    gets(u.name);
+    printf("ID number: ");
+    scanf("%d",&u.id);
+    printf("Phone Number: ");
+    scanf("%s",&u.tel);
+    printf("User type(1 for staff, 0 for ordinary user: ");
+    scanf("%d",&u.is_staff);
+    fwrite(&u, sizeof(struct user), 1, fp);
+    fclose(fp);
+    printf("User successfully added\n");
+}
+
+void view_users() {
+    struct user u;
+    FILE *fp;
+    if ((fp = fopen("users","rb"))==NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    printf("\n\tLIST OF USERS\n\n");
+    int count;
+    for(count = 0;count<65;count++) printf("_");
+    printf("\n");
+    printf("|%8s|\t%20s|\t%10s|\t%8s|\n","ID","NAME","PHONE","IS STAFF");
+    for(count = 0;count<65;count++) printf("_");
+    printf("\n");
+    while(1) {
+        if(feof(fp)) break;
+        fread(&u, sizeof(struct user), 1, fp);
+        printf("|%8d|\t",u.id);
+        printf("%20s|\t",u.name);
+        printf("%8s|\t",u.tel);
+        printf("%8d|\n",u.is_staff);
+    }
+    for(count = 0;count<65;count++) printf("_");
+    printf("\n");
+    fclose(fp);
+
 }
